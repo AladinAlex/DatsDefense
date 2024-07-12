fd6fcb80c5d4a7343a370d327ea8cd9f4c9c012d
Alexandr <89970757+AladinAlex@users.noreply.github.com>
2024-07-12 22:19:45 +0300

    r1

---
+++ Core/Algoritms/FirstAlgo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Algoritms
+{
+    public class FirstAlgo
+    {
+        private HttpCore core;
+        public FirstAlgo()
+        {
+            core = new HttpCore();
+        }
+
+        public async Task MainASync()
+        {
+            Console.WriteLine("Запущен алгоритм 1");
+            // 1. Start Round
+            try
+            {
+                var rStartRound = await core.StartRound();
+                Console.WriteLine($"Успешная регистрация на раунд. До начала: {rStartRound.StartsInSec} секунд.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Провал при регистрации на раунд. Ошибка: {ex.Message}");
+            }
+
+
+            try
+            {
+                var rStatic = await core.StaticMap();
+                Console.WriteLine($"Получение статики карты");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Попытка . Ошибка: {ex.Message}");
+            }
+
+
+        }
+    }
+}
---
+++ Core/HttpCore/HttpCore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Algoritms
+{
+    public class HttpCore
+    {
+        private const string BaseUrl = "https://games.datsteam.dev/";
+        private const string DevBaseUrl = "https://games-test.datsteam.dev/";
+        readonly openapiClient devteamClient;
+        public HttpCore(bool devServer = true)
+        {
+            var httpClient = new HttpClient();
+            if (devServer)
+                httpClient.BaseAddress = new Uri(DevBaseUrl);
+            else
+                httpClient.BaseAddress = new Uri(BaseUrl);
+
+            httpClient.DefaultRequestHeaders.Add("X-Auth-Token", "668ee4f86d598668ee4f86d59b");
+
+            devteamClient = new(httpClient);
+        }
+
+        public async Task<ValidationErrors> Command(Core.Command command)
+        {
+            var result = await devteamClient.CommandAsync(command);
+            return result;
+        }
+
+        public async Task<ParticipateResponse> StartRound()
+        {
+            var result = await devteamClient.ParticipateAsync();
+            return result;
+        }
+
+        public async Task<UnitsResponse> DynamicMap()
+        {
+            return await devteamClient.UnitsAsync();
+        }
+
+        public async Task<MapResponse> StaticMap()
+        {
+            return await devteamClient.WorldAsync();
+        }
+        public async Task<RoundList> RoundList()
+        {
+            return await devteamClient.ZombidefAsync();
+        }
+    }
+}
